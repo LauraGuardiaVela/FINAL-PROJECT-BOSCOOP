1bfcfd75f9052df7a264f0c449aabf50afafa817
LauraGuardiaVela <92033923+LauraGuardiaVela@users.noreply.github.com>
2023-05-09 11:29:35 +0200

    Tots els assets sense música

---
+++ Assets/Scripts/tracking/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] int playerIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setPosition(Vector3 pos)
+    {
+        //swith playerIndex
+        // a les slides
+        Vector3 newPos;
+        switch(playerIndex){
+            case 1: 
+            newPos = new Vector3(Mathf.Clamp(pos.x, 0, 50), pos.y, pos.z);
+            break;
+            case 2: 
+            newPos = new Vector3(Mathf.Clamp(pos.x, 50, 100), pos.y, pos.z);
+            break;
+            case 3: 
+            newPos = pos;
+            break;
+        }
+        transform.position = pos;
+    }
+}
