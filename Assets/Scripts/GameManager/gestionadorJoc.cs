7ea704f32be9856ffefdc686a1fe19965fa70e0e
Carolina del Corral <carolina.cdc@outlook.com>
2023-05-16 11:52:45 +0200

    triggers aigua

--- Assets/Scripts/GameManager/gestionadorJoc.cs
+++ Assets/Scripts/GameManager/gestionadorJoc.cs
@@ -14,7 +14,16 @@ public class gestionadorJoc : MonoBehaviour
     public List<GameObject> camins = new List<GameObject>(); 
 
     //Proves
-    public List<GameObject> roquesProves = new List<GameObject>(); 
+    public List<GameObject> roquesProves = new List<GameObject>();
+
+    // Triggers
+    public List<GameObject> triggersAigua = new List<GameObject>();
+
+    // Aigua
+    public GameObject aigua;
+
+    // Illa
+    public GameObject illa;
 
     // Start is called before the first frame update
     void Start()
@@ -32,47 +41,65 @@ public class gestionadorJoc : MonoBehaviour
     {
         obrintCamins();
         StartCoroutine(elevantRoques());
+        nivellAigua();
     }
     void obrintCamins()
     {
         if(provaLlums.LlumsisWin == true)
         {
-            camins[2].SetActive(true);
+            camins[2].SetActive(true); // camí 3
+            triggersAigua[7].SetActive(false); // trigger camí 3
         }
         if(provaFlors.FlorsisWin == true)
         {
-            camins[3].SetActive(true);
+            camins[3].SetActive(true); // camí 4
+            triggersAigua[8].SetActive(false); // trigger camí 4
         }
         if(provaBolets.boletsIsWin == true)
         {
-            
-            camins[0].SetActive(true);
+            camins[0].SetActive(true); // camí 1
+            triggersAigua[5].SetActive(false); // trigger camí 1
         }
         if(provaVolcans.volcansIsWin == true)
         {
-            camins[1].SetActive(true);
+            camins[1].SetActive(true); // camí 2
+            triggersAigua[6].SetActive(false); // trigger camí 2
         }
         if(provaConjunta.ConjuntaisWin == true)
         {
-            camins[4].SetActive(true);
+            camins[4].SetActive(true); // camí 5
+            triggersAigua[9].SetActive(false); // trigger camí 5
         }   
     }
     IEnumerator elevantRoques()
     {
-        if(camins[0].activeInHierarchy)
+        if(camins[0].activeInHierarchy) // supera prova bolets -> camí 1 s'obre
         {
             yield return new WaitForSeconds(5);
-            roquesProves[1].SetActive(true);
+            roquesProves[1].SetActive(true); // roca flors
+            triggersAigua[3].SetActive(false); // trigger prova flors
         }
-        if(camins[2].activeInHierarchy)
+        if(camins[2].activeInHierarchy) // supera prova llums -> camí 3 s'obre
         {
             yield return new WaitForSeconds(5);
-            roquesProves[3].SetActive(true);
+            roquesProves[3].SetActive(true); // roca volcans
+            triggersAigua[2].SetActive(false); // trigger prova volcans
         }
         if(camins[1].activeInHierarchy && camins[3].activeInHierarchy)
         {
             yield return new WaitForSeconds(5);
-            roquesProves[4].SetActive(true);
+            roquesProves[4].SetActive(true); // roca conjunta
+            triggersAigua[4].SetActive(false); // trigger prova conjunta
+
+        }
+    }
+
+    void nivellAigua()
+    {
+        if (aigua.transform.position.y >= 5.0f)
+        {
+            illa.transform.position = new Vector3(0, -15, 0);
         }
     }
 }
+
